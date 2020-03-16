06c62e6adf2e8e80eaf8fc4cab9bf7befd0b5a16
Santiago <santiago.federiconi@gmail.com>
2020-03-16 22:14:24 +0000

    template

--- express_server.js
+++ express_server.js
@@ -7,18 +7,26 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.set("view engine", "ejs");
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
 
-  app.get("/urls.json", (req, res) => {
-    res.json(urlDatabase);
-  });
-  
-  app.get("/hello", (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n");
-  });
+app.get("/hello", (req, res) => {
+  let templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
