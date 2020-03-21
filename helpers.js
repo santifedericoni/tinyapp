0efb4274104a8e9aca1ee01e89e093d2969e4826
Santiago <santiago.federiconi@gmail.com>
2020-03-21 18:37:16 +0000

    Fixing short url routes, adding comments and reodering the code in express_server.js

--- helpers.js
+++ helpers.js
@@ -1,6 +1,6 @@
+//i get an user and a data base and the return should be all the urls that user create
 const urlsForUser = function(id, urlDatabase) {
   let urlsFromUser = {};
-
   for (let url in urlDatabase) {
     if (urlDatabase[url].userID === id) {
       urlsFromUser[url] = {longURL:urlDatabase[url].longURL, userID: id};
@@ -9,10 +9,10 @@ const urlsForUser = function(id, urlDatabase) {
   return urlsFromUser;
 };
 
-
+// loop until the length of strings i want for the random usigin the characters like a dictionary
 const generateRandomString = function()  {
   let result = '';
-  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'; //this is the "dictionary, if you want any string else, just add it"
   let charactersLength = characters.length;
   for (let i = 0; i < 6; i ++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
